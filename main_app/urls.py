3f920be43ee966c9e9f29bd9ec840b28053bb555
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-10-23 12:17:58 +0300

    fix urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LifeSituationViewSet, ServiceViewSet
+from .views import LifeSituationViewSet, ServiceViewSet, ProcessViewSet
 
 router = DefaultRouter()
 router.register(r'lifesituations', LifeSituationViewSet)
 router.register(r'services', ServiceViewSet)
+router.register(r'processes', ProcessViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

52a26a3c902223b37fe83c7eef31c2794984cf3e
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-10-23 15:25:21 +0300

    fix users

--- main_app/models.py
+++ main_app/models.py
@@ -18,8 +18,7 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.get_full_name()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+
 
 class LifeSituation(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название жизненной ситуации", blank=True, null=True, )

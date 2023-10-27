006b5bc60d8a13a03e66969384433a547667e5bc
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-10-27 11:33:47 +0300

    fix

--- main_app/views.py
+++ main_app/views.py
@@ -11,6 +11,8 @@ from .serializers.service_serializers import ServiceSerializer, ServiceRetrieveS
     ServiceUpdateSerializer
 from .serializers.user_serialzers import UserSerializer, UserRetrieveSerializer
 from .utils import generate_identifier, CustomModelViewSet
+import random
+import string
 
 
 class UserViewSet(CustomModelViewSet):
@@ -20,6 +22,27 @@ class UserViewSet(CustomModelViewSet):
         'retrieve': UserRetrieveSerializer,
     }
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def reset_password(self, request, pk=None):
+        user = self.get_object()
+        print('user', user)
+        email = request.data.get('email')
+        print('email', email)
+        new_password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+        print('new_password', new_password)
+
+        user.set_password(new_password)
+        user.save()
+        print('user.save()', user)
+
+        recipients = mail.send(
+            email, DEFAULT_FROM_EMAIL,
+            subject='Сброс пароля',
+            message=f'Ваш новый пароль: {new_password}',
+            priority='now')
+
+        Response(recipients, status=status.HTTP_201_CREATED)
 
 
 class LifeSituationViewSet(CustomModelViewSet):
@@ -36,13 +59,14 @@ class LifeSituationViewSet(CustomModelViewSet):
     def get_queryset(self):
         user = self.request.user
         organization = user.organization
-        search_string = self.request.query_params.get('search', None)
-
         queryset = LifeSituation.objects.filter(user__organization=organization)
+        return queryset
 
+    def list(self, request, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        search_string = self.request.query_params.get('search', None)
         if search_string:
             queryset = queryset.filter(Q(name__icontains=search_string) | Q(services__name__icontains=search_string))
-
         page = self.paginate_queryset(queryset)
         serializer = LifeSituationListSerializer(page, many=True) if page else LifeSituationListSerializer(queryset,
                                                                                                            many=True)
@@ -64,13 +88,14 @@ class ServiceViewSet(CustomModelViewSet):
     def get_queryset(self):
         user = self.request.user
         organization = user.organization
+        queryset = LifeSituation.objects.filter(user__organization=organization)
+        return queryset
+
+    def list(self, request, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
         search_string = self.request.query_params.get('search', None)
-
-        queryset = Service.objects.filter(user__organization=organization)
-
         if search_string:
-            queryset = queryset.filter(Q(name__icontains=search_string))
-
+            queryset = queryset.filter(Q(name__icontains=search_string) | Q(services__name__icontains=search_string))
         page = self.paginate_queryset(queryset)
         serializer = ServiceRetrieveSerializer(page, many=True) if page else ServiceRetrieveSerializer(queryset,
                                                                                                        many=True)

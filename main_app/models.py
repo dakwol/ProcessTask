2f472bdecc7ce8afdb45bb6fff28615fe727faed
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-10-29 17:55:54 +0300

    fix createsuperuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 
 # Create your models here.
@@ -14,6 +15,34 @@ class Organization(models.Model):
         return self.code
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('email должен быть указан')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
     username = models.CharField(
         max_length=150,
@@ -25,11 +54,13 @@ class CustomUser(AbstractUser):
     organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True,
                                      verbose_name="Организация")
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.get_full_name()
+        return self.email
 
 
 class LifeSituation(models.Model):
